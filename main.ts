d11ad6a63dec122ae2723b3fc6c5e79d22ad0e48
SiraphopSangmee <88707085+erksirr@users.noreply.github.com>
2024-01-17 22:14:13 +0700

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,21 @@
-
+const mysort = (col1:number[], col2:number[]) => {
+    let before:string = col1+","+col2
+    let before_new:string[]=before.split(",")
+    let use:number[]=[]
+    for (let i=0;i<before_new.length;i++){
+        use.push(parseInt(before_new[i]))
+    }
+    for (let i = 0; i < use.length; i++) {
+      for (let j = 0; j < use.length; j++) {
+        let b:number = use[j];
+        if (use[j] > use[j + 1]) {
+          use[j] = use[j + 1];
+          use[j + 1] = b;
+        }
+      }
+    }
+    console.log(use)
+  };
+  let arr1:number[] = [12, 23,78, 576,89]; //duplicate elements
+  let arr2:number[] = [1,2,5,6,90]; //Same element
+  mysort(arr1, arr2);
